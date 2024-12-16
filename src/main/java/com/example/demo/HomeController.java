a3cb393ba191cde3005702d96799baa3a845394b
Kirisailekai <kirisailekai.futatsu@gmail.com>
2024-12-16 16:44:32 +0600

    attempt 17

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
 
     @GetMapping("/")
-    @ResponseBody
     public String home() {
         return "Хочешь молока?";
     }
