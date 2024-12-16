25b753d1e74f1de8685336333a898c22f093b8c1
Kirisailekai <kirisailekai.futatsu@gmail.com>
2024-12-16 16:41:53 +0600

    attempt 16

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -1,11 +1,12 @@
 package com.example.demo;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/")
 public class HomeController {
 
     @GetMapping("/")
